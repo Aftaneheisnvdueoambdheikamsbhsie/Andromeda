7ea9b1b9b3ac622a7f18f7f0971da6e2a0d0b076
aftacreative <aftaam6@gmail.com>
2024-10-12 14:17:58 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0PBtiwbVjUjia1edNbW0UZ0L84rfcLDo",
+  authDomain: "andromediadigital-ddcd0.firebaseapp.com",
+  projectId: "andromediadigital-ddcd0",
+  storageBucket: "andromediadigital-ddcd0.appspot.com",
+  messagingSenderId: "721176765748",
+  appId: "1:721176765748:web:yourAppId" // Ganti dengan appId jika perlu
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
