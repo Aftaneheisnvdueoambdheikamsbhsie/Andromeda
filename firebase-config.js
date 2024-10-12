a803e83881b1af300cfa4691f75ca19b7b3fcc29
aftacreative <aftaam6@gmail.com>
2024-10-12 18:44:33 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,23 @@
+// firebase-config.js
+
+// Import Firebase modules
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push } from "firebase/database";
+
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB0PBtiwbVjUjia1edNbW0UZ0L84rfcLDo",
-    authDomain: "andromediadigital-ddcd0.firebaseapp.com",
-    projectId: "andromediadigital-ddcd0",
-    storageBucket: "andromediadigital-ddcd0.appspot.com",
-    messagingSenderId: "721176765748",
-    appId: "1:721176765748:web:yourAppId" // Ganti dengan appId yang benar
+  apiKey: "AIzaSyB0PBtiwbVjUjia1edNbW0UZ0L84rfcLDo",
+  authDomain: "andromediadigital-ddcd0.firebaseapp.com",
+  databaseURL: "https://andromediadigital-ddcd0-default-rtdb.firebaseio.com",
+  projectId: "andromediadigital-ddcd0",
+  storageBucket: "andromediadigital-ddcd0.appspot.com",
+  messagingSenderId: "721176765748",
+  appId: "1:721176765748:web:985df3d56d4f39fcb03161",
+  measurementId: "G-L23NM7B6SZ"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database, ref, push };
